39d58d0f37b7a81c5edb5808dedafecf75940638
karasumi <gakusyu2019@outlook.jp>
2019-09-01 13:13:07 +0000

    カラムステータスの修正

---
+++ db/migrate/20190901125610_remove_status_to_task.rb
@@ -0,0 +1,4 @@
+class RemoveStatusToTask < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_31_140620) do
+ActiveRecord::Schema.define(version: 2019_09_01_125610) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
